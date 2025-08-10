cd0a03a1bd4daf1b608fdca5d55cb67dc6dbb889
Melissa Setzer <mat704@gmail.com>
2025-08-10 19:18:35 -0400

    got rid of unneeded constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,26 +1,7 @@
 package frc.robot;
 
-import static edu.wpi.first.units.Units.Degree;
-import static edu.wpi.first.units.Units.Inch;
 import static edu.wpi.first.units.Units.MetersPerSecond;
-import static edu.wpi.first.units.Units.Radian;
-
-import edu.wpi.first.math.Matrix;
-import edu.wpi.first.math.VecBuilder;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.math.numbers.N1;
-import edu.wpi.first.math.numbers.N3;
-import edu.wpi.first.units.measure.Angle;
-import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.units.measure.LinearVelocity;
-import frc.robot.helpers.PID;
-
-import java.util.Arrays;
-import java.util.Map;
 
 /** Constants used throughout the robot code. */
 public final class Constants {
@@ -66,17 +47,8 @@ public final class Constants {
     /** Power level for dropping coral */
     public static final double DROP_POWER = 1.0;
 
-    /** Power level for ejecting stuck objects */
-    public static final double EJECT_POWER = 0.5;
-
     /** Whether manipulator motor is inverted */
     public static final boolean INVERTED = false;
-
-    /** Current limit for manipulator motor in amps */
-    public static final int CURRENT_LIMIT = 20;
-
-    /** Ramp rate for manipulator motor (seconds from 0 to full throttle) */
-    public static final double RAMP_RATE = 0.1;
 
     /** PCM ID for the Intake pneumatics. */
     public static final int PCM_ID = 14;
